4ae8d115cbb4f96f5b617ceb1ad6ae481a17815e
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-11-27 23:05:10 -0500

    Create index.php

---
+++ var/www/html/index.php
@@ -0,0 +1,25 @@
+<title>Landing Page</title>
+<div class="alert alert-success" role="alert">
+  <h4 class="alert-heading">Message from Administrators!</h4>
+  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
+  <hr>
+  <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+</div> 
+<hr size="10">
+<h2> Simple Project Hosting</h2>
+
+CLIENT I.P: <?php echo $_SERVER['REMOTE_ADDR']; ?>
+<br>
+Requested URL: <?php echo $_SERVER['REQUEST_URI']; ?>
+<br>
+
+PORT: <?php echo $_SERVER['SERVER_PORT']; ?>
+
+
+
+
+<script> console.log("Landing Page: Rossm Devs, Inc.")</script>
+
+
+<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
