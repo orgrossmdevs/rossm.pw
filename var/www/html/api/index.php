c4eff4c3f5c4cf7973f5cd19fb4334e5bf818738
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-11-27 23:18:24 -0500

    Create index.php

---
+++ var/www/html/api/index.php
@@ -0,0 +1,13 @@
+<title>API</title>
+<h4> API DOWN</h4>
+<html>
+CLIENT I.P: <?php echo $_SERVER['REMOTE_ADDR']; ?>
+<br>
+Requested URL: <?php echo $_SERVER['REQUEST_URI']; ?>
+<br>
+
+PORT: <?php echo $_SERVER['SERVER_PORT']; ?>
+
+
+</html>
+
