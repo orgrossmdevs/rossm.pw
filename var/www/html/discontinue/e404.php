ba5e3e1366d93c03b268af610bfc65db9b2dfab8
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-11-27 23:14:04 -0500

    Create e404.php

---
+++ var/www/html/discontinue/e404.php
@@ -0,0 +1,15 @@
+<title>404</title>
+<h2> Requested URL Not Found</h2>
+
+
+CLIENT I.P: <?php echo $_SERVER['REMOTE_ADDR']; ?>
+<br>
+Requested URL: <?php echo $_SERVER['REQUEST_URI']; ?>
+<br>
+
+PORT: <?php echo $_SERVER['SERVER_PORT']; ?>
+
+
+
+
+<script> console.log("404 Page: Rossm Devs, Inc.")</script>
