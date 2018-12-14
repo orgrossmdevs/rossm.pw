dabd773871b75b371e14c4344d7ad8861cc7745d
Ross M <37253938+RossMdevs@users.noreply.github.com>
2018-12-13 20:53:32 -0500

    Include new TAWK.TO livechat script loader.
    Added the support for Livechat (Tawk.to)

--- var/www/html/index.php
+++ var/www/html/index.php
@@ -14,7 +14,9 @@ PORT: <?php echo $_SERVER['SERVER_PORT']; ?>
  <div class="footer">Ross M, Devs 2017-18 &copy; | <span class="glyphicon glyphicon-pencil"></span> <a href="https://github.com/orgrossmdevs/rossm.pw">Edit this page on github</a></div>
 
 	
-	
+<script type="text/javascript" href="#REPLACE_WITH_LIVECHAT_JS_URL">
+</script>
+
 	
 	
 <link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
